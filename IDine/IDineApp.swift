4c599f572ab674fc2bbee09152fbdfda9313a07e
Nerto Kageh <kagehnerto@yahoo.com>
2022-09-06 21:58:07 -0500

    Initial Commit

---
+++ IDine/IDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  IDineApp.swift
+//  IDine
+//
+//  Created by Induran Aderinto  on 9/6/22.
+//
+
+import SwiftUI
+
+@main
+struct IDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
