bf4d0e9c15bc28696a331c956e85124c823927ea
Nerto Kageh <kagehnerto@yahoo.com>
2022-09-10 13:53:26 -0500

    Composed views to create a List Row. Images of the food and their price can now be seen on the menu.

---
+++ IDine/ItemRow.swift
@@ -0,0 +1,29 @@
+//
+//  ItemRow.swift
+//  IDine
+//
+//  Created by Induran Aderinto  on 9/10/22.
+//
+
+import SwiftUI
+
+struct ItemRow: View {
+    let item: MenuItem
+    
+    var body: some View {
+        HStack {
+            Image(item.thumbnailImage)
+            
+            VStack(alignment: .leading) {
+                Text(item.name)
+                Text("$\(item.price)")
+            }
+        }
+    }
+}
+
+struct ItemRow_Previews: PreviewProvider {
+    static var previews: some View {
+        ItemRow(item: MenuItem.example)
+    }
+}
